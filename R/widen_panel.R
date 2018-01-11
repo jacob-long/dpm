4f068fcb73b53009638e0d71ca4049b61bba0b41
Jacob Long <jlongrc@gmail.com>
2018-01-11 09:49:57 -0500

    Play nicely with panelr

--- R/widen_panel.R
+++ R/widen_panel.R
@@ -1,16 +1,13 @@
 #' @importFrom stats complete.cases reshape
 
-widen_panel <- function(data, id = NULL, wave = NULL, varying, constants) {
+widen_panel <- function(data, varying, constants) {
 
-  if (class(data)[1] == "panel_data" & is.null(id) & is.null(wave)) {
-    id <- attr(data, "id")
-    wave <- attr(data, "wave")
-  }
+  data <-
+    as.data.frame(data[names(data) %in% c("id", "wave", varying, constants)])
 
-  data <- data[,names(data) %in% c(id, wave, varying, constants)]
-
-  data <- suppressWarnings(reshape(data = data, v.names = varying, timevar = wave,
-                  idvar = id, direction = "wide", sep = "_"))
+  data <-
+    suppressWarnings(reshape(data = data, v.names = varying, timevar = "wave",
+                  idvar = "id", direction = "wide", sep = "_"))
 
   complete_obs <- sum(complete.cases(data))
   attr(data, "complete_obs") <- complete_obs
