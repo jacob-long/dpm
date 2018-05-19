d7884ef95637bcef7e386adbcad7d140e31f0d5d
Jacob Long <jlongrc@gmail.com>
2018-05-19 19:51:23 -0400

    Tone down startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 # Pkg startup message
 .onAttach <- function(libname, pkgname) {
-  msg <- "This is BETA software. Expect bugs and missing functionality.
-You should cross-reference results with xtdpdml for Stata if unsure. Go to
+  msg <- "dpm is in-development software. You should consider cross-referencing
+results with xtdpdml for Stata if possible. Go to
 https://www3.nd.edu/~rwilliam/dynamic/ to learn about xtdpdml and the
 underlying method."
   packageStartupMessage(msg)
