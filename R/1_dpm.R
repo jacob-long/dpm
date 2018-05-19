73a5711f6e60c2f9e78a2b374d25b49039f4206d
Jacob Long <jlongrc@gmail.com>
2018-05-19 16:50:38 -0400

    Add dpm class definition

---
+++ R/1_dpm.R
@@ -0,0 +1,17 @@
+#### dpm class ##################################
+#' @title Dynamic Panel Model (`dpm`) class
+#' @description Models fit using [dpm()] return values of this class, which
+#'  inherits from \code{\link[lavaan]{lavaan-class}}.
+#' @slot call_info A list of metadata about the arguments used.
+#' @slot call The actual function call.
+#' @slot mod_string The model formula passed to `lavaan`.
+#' @slot wide_data The data provided to the `data` argument in the function
+#'  call.
+#' @export
+setClass("dpm",
+         contains = "lavaan",
+         slots = c("call_info" = "list",
+                   "call" = "call",
+                   "mod_string" = "character",
+                   "wide_data" = "data.frame")
+)
