151567f328c48bc5ab2a3c158d3f90f021f58fbe
Jacob Long <jlongrc@gmail.com>
2018-08-15 13:49:35 -0400

    Use panelr::widen_panel rather than reshape

--- R/widen_panel.R
+++ R/widen_panel.R
@@ -2,14 +2,14 @@
 
 widen_panel_in <- function(data, varying, constants) {
 
-  id <- panelr::get_id(data)
-  wave <- panelr::get_wave(data)
-  data <-
-    as.data.frame(data[names(data) %in% c(id, wave, varying, constants)])
+  # id <- panelr::get_id(data)
+  # wave <- panelr::get_wave(data)
+  # data <-
+  #   as.data.frame(data[names(data) %in% c(id, wave, varying, constants)])
 
-  data <-
-    suppressWarnings(reshape(data = data, v.names = varying, timevar = wave,
-                  idvar = id, direction = "wide", sep = "_"))
+  data <- panelr::widen_panel(data, varying = varying)
+    # suppressWarnings(reshape(data = data, v.names = varying, timevar = wave,
+    #               idvar = id, direction = "wide", sep = "_"))
 
   complete_obs <- sum(complete.cases(data))
   attr(data, "complete_obs") <- complete_obs
