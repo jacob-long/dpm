376aed41bf1804adbbf414d44d2c1b64e27408ba
Jacob Long <jlongrc@gmail.com>
2019-05-17 16:42:24 -0400

    Drop startup message

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-# Pkg startup message
-.onAttach <- function(libname, pkgname) {
-  msg <- "dpm is in-development software. You should consider cross-referencing
-results with xtdpdml for Stata if possible. Go to
-https://www3.nd.edu/~rwilliam/dynamic/ to learn about xtdpdml and the
-underlying method."
-  packageStartupMessage(msg)
-}
